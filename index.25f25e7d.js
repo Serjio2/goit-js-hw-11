4e1c11c7da55f0a07afafeafecb9a9ddcf512ca6
Serjio2 <130094175+Serjio2@users.noreply.github.com>
2023-07-06 22:56:03 +0000

    Deploying to gh-pages from @ Serjio2/goit-js-hw-11@7bf4a9d60e1b0c7c44bf25a6d48533c869157091 🚀

--- index.25f25e7d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-form");console.log(e),e.addEventListener("submit",(function(e){e.preventDefault();const o=e.target[0].value;console.log("I`m fetch"),console.log(o)}));
-//# sourceMappingURL=index.25f25e7d.js.map
--- index.7b2c13ef.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-form");console.log(e),e.addEventListener("submit",(function(e){e.preventDefault();var o=e.target[0].value;console.log("I`m fetch"),console.log(o)}))}();
-//# sourceMappingURL=index.7b2c13ef.js.map
