7bf4a9d60e1b0c7c44bf25a6d48533c869157091
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-07-07 01:55:35 +0300

    Second day coding

--- src/index.js
+++ src/index.js
@@ -1,34 +1,71 @@
+import GetImageAPI from './js/get-image';
 
-import { getImage, qwery } from './js/get-image';
-
+const getimageApiInstance = new GetImageAPI();
 
 const searchInputFormEl = document.querySelector('.search-form');
+const createGalleryEl = document.querySelector('.gallery');
 
-console.log(searchInputFormEl);
 
+// console.log(galleryInfoEl);
 
 searchInputFormEl.addEventListener('submit', heandleSearchBtn);
 
-
 function heandleSearchBtn(event) {
-    event.preventDefault();
-    
-    qwery = event.target[0].value;
-    
-  console.log(qwery)
-  
+  event.preventDefault();
 
-  getImage().then(data => {
+  getimageApiInstance.query = event.target.elements.searchQuery.value;
 
-    for(const item of data.hits) {
+  // console.log(getimageApiInstance.query);
+  createGalleryEl.innerHTML = '';
 
-        const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = item;
+  getimageApiInstance
+    .getImage()
+    .then(data => {
+      for (const item of data.hits) {
+        const {
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        } = item;
 
-    // console.log(webformatURL)
-    }
+        createGalleryEl.insertAdjacentHTML(
+          'beforeend',
+          `
+          <div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          
+          <div class="info">
+            <p class="info-item">
+              <b>Likes${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads${downloads}</b>
+            </p>
+          </div>
+        </div>
+        `
+        );
 
-  
-    }
-  )
+        // const galleryInfoEl = document.querySelector('.info');
+        // galleryInfoEl.classList.add('card-info')
+        // console.log(galleryInfoEl);
+        // console.log(item);
+
+      }
+    })
+    .catch(() => {
+      console.log(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    });
 }
-
--- src/js/get-image.js
+++ src/js/get-image.js
@@ -1,24 +1,34 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38070377-8dbe611b16263765ee807d393';
 
-const qwery = null;
+// const qwery = null; 
 const imageType = 'photo';
 const orientation = 'horizontal'
 const safesearch = true;
 
 
+ export default class GetImageAPI {
+  constructor () {
+    this.searchQuery = '';
+  }
 
-function getImage() {
-  return fetch(`${BASE_URL}?key=${API_KEY}&q=${qwery}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}`)
+ getImage() {
+  return fetch(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}`)
   .then((response) => {
     if (!response.ok) {
         throw new Error(response.status)
     }
-    return response.json()
-  })
+    return response.json();
+    })
+  }
 
-// console.log('I`m fetch');
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery
+  }
 }
 
 
-export { getImage, qwery };
