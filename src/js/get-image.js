e35bb61fe451641c4d1a51efca40d9fdd66b9e63
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-07-11 02:27:40 +0300

    add axios and async

--- src/js/get-image.js
+++ src/js/get-image.js
@@ -1,36 +1,32 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['Authorization'] = '38070377-8dbe611b16263765ee807d393';
 
+export default class GetImageAPI {
 
+   BASE_URL = 'https://pixabay.com/api/';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '38070377-8dbe611b16263765ee807d393';
-
-
-const imageType = 'photo';
-const orientation = 'horizontal'
-const safesearch = true;
-
-
- export default class GetImageAPI {
   constructor () {
     this.searchQuery = '';
     this.page = 1;
     this.perPage = 40;
   }
 
- getImage() {
-  // console.log(this);
-  return fetch(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&per_page=${this.perPage}&page=${this.page}`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status)
-    }
-    return response.json();
-  })
-  
+ async getImage() {
+  const response = await axios.get(`${this.BASE_URL}`, {
+     params: {
+       key: '38070377-8dbe611b16263765ee807d393',
+       q: `${this.searchQuery}`,
+       imageType: 'photo',
+       orientation: 'horizontal',
+       safesearch: true,
+       page: `${this.page}`,
+       perPage: `${this.perPage}`,
+     },
+     headers: {
+       'Content-Type': 'application/json',
+     }
+   });
+   return response.data;
 }
 
   incrementPage() {
